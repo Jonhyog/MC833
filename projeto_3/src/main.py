408f9ea44857250763ab112256d564f32047906d
João Miguel <jm.og@hotmail.com>
2024-06-13 15:59:39 -0300

    process multiple files and fixes thoughput, count and host getters

--- projeto_3/src/main.py
+++ projeto_3/src/main.py
@@ -1,6 +1,7 @@
-from scapy.all import rdpcap, ls, IP, ICMP
+from scapy.all import rdpcap, IP, ls, ICMP, ARP
 from decimal import Decimal
 from typing import Dict, List
+import argparse
 
 class ICMPMetrics:
     def __init__(self, raw_pcap):
@@ -17,13 +18,13 @@ class ICMPMetrics:
             data = hosts.get(src)
 
             if data:
-                hosts[src] = (data[0], pkt, data[2] + len(pkt))
+                hosts[src] = (data[0], pkt, data[2] + len(pkt[ICMP]))
             else:
-                hosts[src] = (pkt, None, len(pkt))
+                hosts[src] = (pkt, None, len(pkt[ICMP]))
 
         # Calculates avg throughput for each host    
         for host, data in hosts.items():
-            throughput[host] = data[2] / (data[1].time - data[0].time)
+            throughput[host] = (data[2] * 8) / (data[1].time - data[0].time)
 
         return throughput
 
@@ -48,46 +49,68 @@ class ICMPMetrics:
 
         return delay
 
-    def get_count(self) -> Dict[str, int]:
+    def get_count(self) -> Dict[str, Dict[str, int]]:
         count = {}
 
-        # Gets number of pkts sent by each host
-        for pkt in self.prc_pcap:
-            src = pkt[IP].src
-            data = count.get(src)
-
-            if data:
-                count[src] = count[src] + 1
+        for pkt in self.raw_pcap:
+            if ICMP in pkt:
+                src = pkt[IP].src
+                pkt_t = "ICMP"
+            elif ARP in pkt:
+                src = pkt[ARP].psrc
+                pkt_t = "ARP"
             else:
-                count[src] = 1
+                print("Found unrecognized pkt type.\nTerminating...")
+                exit(1)
+            
+            data = count.get(src)
+            if not data: count[src] = {}
 
+            t_count = count[src].get(pkt_t)
+            count[src][pkt_t] = 1 if t_count is None else count[src][pkt_t] + 1
+        
         return count
 
-    def get_hosts(self) -> List[str]:
-        return list(set(map(lambda pkt: pkt[IP].src, self.prc_pcap)))
+    def get_hosts(self) -> Dict[str, List]:
+        srcs, dsts = [], []
+
+        # gets ping src and dst using echo (type == 8) and echo reply (type == 0)
+        # ICMP type field value
+        for pkt in self.prc_pcap:
+            if pkt[ICMP].type == 8 and pkt[IP].src not in srcs:
+                srcs.append(pkt[IP].src)
+            elif pkt[ICMP].type == 0 and pkt[IP].src not in dsts:
+                dsts.append(pkt[IP].src)
+
+        return {"sources": srcs, "destinies": dsts}
 
 def main():
-    FILE = "data/h1-h3.pcap"
-    raw_pcap = rdpcap(FILE)
-    metrics = ICMPMetrics(raw_pcap)
-    data = {
-        "throughput": (metrics.get_throughput(), "B/s"),
-        "delay": (metrics.get_delay(), "s"),
-        "count": (metrics.get_count(), ""),
-        "hosts": (metrics.get_hosts(), ""),
-    }
+    parser = argparse.ArgumentParser(description='Process pcap file')
+    parser.add_argument("filename", type=str, nargs='+')
+    args = parser.parse_args()
+    files = args.filename
 
-    print(f"Analysing file {FILE}:")
-    for key, value in data.items():
-        print(f"Calculating {key}:")
+    for file in files:
+        raw_pcap = rdpcap(file)
+        metrics = ICMPMetrics(raw_pcap)
 
-        value, unit = value
-        if isinstance(value, dict):
-            print("\n".join([f"\t{dk}: {dv:.4f} {unit}" for dk, dv in value.items()]))
-        else:
-            print("\n".join([f"\t{x}" for x in value]))
+        print(f"Analysing \"{file}\"")
+        print()
 
-    # print(f"{len(pcap)=} {len(raw_pcap)=}")
+        print("Listing Hosts:")
+        print("\n".join([f"\t{dk}: {''.join(dv)}" for dk, dv in metrics.get_hosts().items()]))
+
+        print("Average Throughput:")
+        print("\n".join([f"\t{dk}: {dv:.4f} b/s" for dk, dv in metrics.get_throughput().items()]))
+
+        print("Average Time Between Packets:")
+        print("\n".join([f"\t{dk}: {dv:.4f} s" for dk, dv in metrics.get_delay().items()]))
+
+        count = metrics.get_count()
+        print("Listing Number of Packets Sent:")
+        print("\n".join([f"\t{dk}: {dv}" for dk, dv in count.items()]))
+
+        print("-" * 48)
 
     
 if __name__ == "__main__":
